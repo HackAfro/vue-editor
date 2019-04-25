3ac2f3dca0900ac3742b27d49f3f749fcfb449cd
Richard Afro <richyafro@hotmail.com>
2019-04-25 16:32:30 +0100

    Use kendo UI editor plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import '@progress/kendo-ui'
+import '@progress/kendo-theme-default/dist/all.css'
+import { EditorInstaller } from '@progress/kendo-editor-vue-wrapper'
 import App from './App.vue'
 
+Vue.use(EditorInstaller)
 Vue.config.productionTip = false
 
 new Vue({
